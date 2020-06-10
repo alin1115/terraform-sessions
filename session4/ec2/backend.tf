b59b6a1aea489a2d82cf5bd188222531ce707aad
alibek17 <alybekkalyk@gmail.com>
2020-06-10 01:12:26 -0500

    added backend

---
+++ session4/ec2/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3" {
+        bucket = "akumo-terraform101-state"
+        key    = "session4/ec2.tfstate"
+        region = "us-east-1"
+        dynamodb_table = "terraform101-state-locks"
+        encrypt = true
+    }
+}
